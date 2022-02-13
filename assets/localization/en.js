7ee38ee7ef080c03eb93c24dfd13d6acdcdc8c35
Murti Daryandono <murti.daryandono@gmail.com>
2022-02-13 20:08:18 +0700

    Init commit

---
+++ assets/localization/en.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){localize({"#modal-search .modal-content input&placeholder":"Keywords...","#modal-search .modal-footer a&html":"Search","#modal-infos h4&html":"Page informations","#modal-infos p strong:nth-of-type(1)&html":"Title :","#modal-infos p strong:nth-of-type(2)&html":"Language :","#modal-infos p strong:nth-of-type(3)&html":"Last modification :"})});
